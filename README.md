55a87330d980e65addbd31c28e586880326484f5
Humphrey Ahn <ahnsv@bc.edu>
2018-11-18 14:15:59 +0900

    oauth added, need to figure out how to get snappay Google secret key

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ SnapPay go server
 - logging
 - more elegant data form
 - concurrency issues
+
+## References
+- [https://mingrammer.com/getting-started-with-oauth2-in-go/](https://mingrammer.com/getting-started-with-oauth2-in-go/)
--- oauth/oauth.go
+++ oauth/oauth.go
@@ -1,11 +1,45 @@
 package oauth
 
 import (
+	"crypto/rand"
+	"github.com/ahnsv/snappay-server/config"
 	"github.com/gorilla/sessions"
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
+const (
+	CallBackURL = "http://localhost:1333/oauth/callback"
+
+	// 인증 후 유저 정보를 가져오기 위한 API
+	UserInfoAPIEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo"
+
+	// 인증 권한 범위. 여기에서는 프로필 정보 권한만 사용
+	ScopeEmail   = "https://www.googleapis.com/auth/userinfo.email"
+	ScopeProfile = "https://www.googleapis.com/auth/userinfo.profile"
+)
+
+var OAuthConfig *oauth2.Config
+
 func init() {
-	sessions.NewSession()
-	oauth2.NewClient()
+	c := config.GetConfig()
+	OAuthConfig := &oauth2.Config{
+		ClientID:     c.GetString("client_id"),
+		ClientSecret: c.GetString(""),
+		RedirectURL:  CallBackURL,
+		Scopes:       []string{ScopeEmail, ScopeProfile},
+		Endpoint:     google.Endpoint,
+	}
+}
+
+// state 값과 함께 Google 로그인 링크 생성
+func GetLoginURL(state string) string {
+	return OAuthConfig.AuthCodeURL(state)
+}
+
+// 랜덤 state 생성기
+func RandToken() string {
+	b := make([]byte, 32)
+	rand.Read(b)
+	return base64.StdEncoding.EncodeToString(b)
 }
